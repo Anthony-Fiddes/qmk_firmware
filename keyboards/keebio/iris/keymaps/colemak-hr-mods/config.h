3f18183b8fe41bcc03fc96628ece74ba7efea4a1
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2021-09-15 20:42:09 -0400

    Fix caps lock, adjust tapping term, move layer toggles out one key

--- keyboards/keebio/iris/keymaps/colemak-hr-mods/config.h
+++ keyboards/keebio/iris/keymaps/colemak-hr-mods/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EE_HANDS
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 165
+#define TAPPING_TERM 175
 
 #define TAPPING_TERM_PER_KEY
 
--- keyboards/keebio/iris/keymaps/colemak-hr-mods/keymap.c
+++ keyboards/keebio/iris/keymaps/colemak-hr-mods/keymap.c
@@ -18,28 +18,33 @@
 #define HOME_E RSFT_T(KC_E)
 #define HOME_I LALT_T(KC_I)
 #define HOME_O RGUI_T(KC_O)
+#define HOME_ENT LT(_NAV, KC_ENT)
 #define HOME_SPC LT(_NAV, KC_SPC)
 
 #define TOG_GAME TG(_QWERTY)
 
 // Tap dance indices
 enum {
-    // Home Tab
-    H_TAB = 0,
+    // Home OS
+    H_LOS = 0,
     // Home Enter
-    H_ENT,
+    H_RAL,
     // Toggle game layer on double tap, rshift when held
     GAME,
+    // Usually caps lock, double tap asks magic boot to please make caps lock work normally.
+    CAPS,
+    // Brackets
     PRN,
     CBRK,
     BRC,
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [H_TAB] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_TAB, _NUMS),
-  [H_ENT] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_ENT, _NAV),
+  [H_LOS] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_LGUI, _NUMS),
+  [H_RAL] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_RALT, _NAV),
   [GAME] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_RSFT, _QWERTY),
   [PRN] = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_RPRN),
+  [CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_CAPS, CL_CAPS),
   [CBRK] = ACTION_TAP_DANCE_DOUBLE(KC_LCBR, KC_RCBR),
   [BRC] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
 };
@@ -48,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_COLEMAK] = LAYOUT(
       //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LOCK,
+         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TD(CAPS),
       //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
          KC_GRV,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                               KC_J,    KC_L,    KC_U,     KC_Y,   KC_SCLN, KC_BSLS,
       //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -56,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
          KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   HOME_BSPC,         HOME_SPC, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,TD(GAME),
       //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                        KC_LGUI,TD(H_TAB),HOME_BSPC,                 HOME_SPC,TD(H_ENT),KC_RALT
+                                       TD(H_LOS),HOME_TAB,HOME_BSPC,                 HOME_SPC,HOME_ENT,TD(H_RAL)
       //                               └────────┴────────┴────────┘                 └────────┴────────┴────────┘
 ),
 
@@ -76,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_NAV] = LAYOUT(
     //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-       KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F4,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_LOCK,
+       KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F4,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        RGB_TOG, BL_STEP, KC_VOLU, KC_VOLD, KC_MUTE, _______,                            KC_PSCR, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_ACL2,
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -105,6 +110,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+        case TD(H_LOS):
+        case TD(H_RAL):
         case HOME_BSPC:
         case HOME_SPC:
             return TAPPING_TERM + 90;
