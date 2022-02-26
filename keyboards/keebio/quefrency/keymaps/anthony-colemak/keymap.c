99a715d1a6fa1bf5b62a57bde6c7003425e78599
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2022-02-25 23:47:00 -0500

    Add media controls to NAV layer

--- keyboards/keebio/quefrency/keymaps/anthony-colemak/keymap.c
+++ keyboards/keebio/quefrency/keymaps/anthony-colemak/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NAV] = LAYOUT_65_with_macro(
     _______, _______, KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, _______, \
-    _______, _______, _______, KC_END,  KC_PGDN, KC_PGUP, KC_HOME, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______, _______, _______, _______, \
+    _______, _______, _______, KC_MPRV, KC_VOLU, KC_VOLD, KC_MNXT, KC_MPLY, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______, _______, _______, _______, \
     _______, _______, KC_ESC,  KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______, _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _______, _______, _______, \
     _______, RGB_TOG, KC_CAPS, _______, KC_BTN3, KC_BTN2, KC_BTN1, _______, _______, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R, _______, _______, _______, \
     RESET,   RGB_MOD, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
