32308e38204ebaaef559c0b4858db38797cc6cfb
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2022-06-24 22:09:10 -0700

    Hide reset behind a double tap!
    Also shift over gaming layer so it aligns with the other layers.

--- keyboards/keebio/iris/keymaps/colemak-hr-mods/keymap.c
+++ keyboards/keebio/iris/keymaps/colemak-hr-mods/keymap.c
@@ -39,6 +39,8 @@ enum {
     GAME,
     // Usually shift, double tap for caps.
     SH_C,
+	// Usually shift, double tap to reset
+    RST,
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
@@ -46,6 +48,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [H_RAL] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_RALT, _NAV),
   [GAME] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_RSFT, _QWERTY),
   [SH_C] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+  [RST] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, RESET),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -72,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_ESC,  KC_EQL,  LBRC,    LCBR,    LPRN,    KC_UNDS,                            KC_DOT,  KC_4,    KC_5,    KC_6,    KC_MINS, KC_AMPR,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-       RESET,   KC_CIRC, KC_RBRC, KC_RCBR, KC_RPRN, KC_SLSH, _______,          _______, KC_COMM, KC_1,    KC_2,    KC_3,    KC_PLUS, KC_ASTR,
+       TD(RST),   KC_CIRC, KC_RBRC, KC_RCBR, KC_RPRN, KC_SLSH, _______,          _______, KC_COMM, KC_1,    KC_2,    KC_3,    KC_PLUS, KC_ASTR,
     //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                       _______, _______, _______,                   _______, KC_0,    KC_0
     //                               └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -86,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_ESC,  KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_APP,                             KC_BTN2, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_ACL1,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-       RESET,   RGB_TOG, RGB_MOD, KC_MPRV, KC_MPLY, _______, _______,          _______, KC_BTN1, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R, KC_ACL0,
+       TD(RST), RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______,          _______, KC_BTN1, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R, KC_ACL0,
     //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                       _______, _______, _______,                   _______, _______, _______
     //                               └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -94,13 +97,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
     //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
+       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_SPC,           KC_SPC,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TOG_GAME,
+       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,           KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TOG_GAME,
     //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                       _______, _______, KC_SPC,                    KC_SPC,  _______, _______
     //                               └────────┴────────┴────────┘                 └────────┴────────┴────────┘
